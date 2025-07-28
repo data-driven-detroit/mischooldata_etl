771349b9d91891464d078fd46be2866fff127f54
Mike V <mike@datadrivendetroit.org>
2025-07-28 16:57:35 -0400

    chunking the geocoding step

--- mischooldata_etl/eem/geocode.py
+++ mischooldata_etl/eem/geocode.py
@@ -11,18 +11,24 @@ def geocode_schools():
     field_reference = json.loads(
         (WORKING_DIR / "conf" / "field_reference_2010_2025.json").read_text()
     )
-
-    all_entities = pd.read_csv(
-        WORKING_DIR / "output" / "combined_years.csv",
-        dtype=field_reference["in_types"]
-    )
-
-    schools = all_entities[all_entities["building_code"] != '00000']
     
-    geocoded = batch_geocode(schools, id_column="building_code", 
-                             address="address", city="city", state="state", 
-                             zip="zip_code", as_gdf = True)
+    result = []
+    for i, chunk in enumerate(pd.read_csv(
+        WORKING_DIR / "output" / "combined_years.csv",
+        dtype=field_reference["in_types"],
+        chunksize=5_000
+    ), start=1):
+        print(f"Geocoding chunk # {i}")
 
+        schools = chunk[chunk["building_code"] != '00000']
+        batch = batch_geocode(schools, id_column="building_code", 
+                                 address="address", city="city", state="state", 
+                                 zip="zip_code", as_gdf = True)
+
+        result.append(batch)
+    
+
+    geocoded = pd.concat(result)
     geocoded.rename({
         "id": "building_code" # put this back to the original name
     }).to_file(WORKING_DIR / "output" / "geocoded_schools.geojson")
